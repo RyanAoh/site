a54913e4fdcf61b5c1040fbb8c5470ec06d69658
RyanAoh <zim8023@sina.com>
2018-10-17 00:11:16 +0800

    test vue

--- js/script.js
+++ js/script.js
@@ -12,3 +12,60 @@ $('#cssmenu').prepend('<div id="menu-button">Menu</div>');
 	});
 });
 } )( jQuery );
+
+
+
+new Vue({
+  el: '#artiles',
+  created:{
+
+  },
+  data: {
+    articles:[
+      {
+      	title:"python学习笔记",
+      	pub_time:"2018-10-16 15:55:20",
+      	category:{
+      		id:1,
+      		name:"编程开发"
+      	},
+      	tag:[
+      	{
+      		id:1,
+      		name:"python"
+      	},
+      	{
+      		id:2,
+      		name:"django"
+      	}
+      	],
+      	read_num:90,
+      	article:"In order to receive the best answers to your questions please follow these simple guidelines.\
+Be descriptive about your question or problem and be sure to include any errors. Single line topics will rarely receive answers and do not just post a screenshot.\
+Provide examples of the code in question and use syntax highlighting.\
+Breakdown the issue in your code and provide a link to an example that replicates it. Use JSFiddle, CodePen, JSBin, or CodeSandbox.\
+Following these few steps will greatly improve your chances of getting a good quality answer to your issue. Remember, everyone is here to share ideas and to help out using their own free time. Making an effort to craft a well thought out topic pays tribute to this."
+      },
+      {
+      	title:"python学习笔记",
+      	pub_time:"2018-10-16 15:55:20",
+      	category:{
+      		id:1,
+      		name:"编程开发"
+      	},
+      	tag:[
+      	{
+      		id:1,
+      		name:"python"
+      	},
+      	{
+      		id:2,
+      		name:"django"
+      	}
+      	],
+      	read_num:90,
+      	article:"In order to receive the best answers to your questions please follow these simple guidelines."
+      }
+    ]
+  }
+})
